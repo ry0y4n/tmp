21a8a1cbb17eb6acd1c6462ed7f6eb89cc115eac
momosuke <rusk.ryoy4n@gmail.com>
2023-03-20 16:31:18 +0900

    add hoge

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const urlInput = document.getElementById('url')
-const downloadButton = document.getElementById('download')
+const startTimeInput = document.getElementById('start-time');
+const durationInput = document.getElementById('duration');
+const urlInput = document.getElementById('url');
+const downloadButton = document.getElementById('download');
 
 downloadButton.addEventListener('click', () => {
-  const url = urlInput.value
-  const downloadUrl = `http://127.0.0.1:8080/download?url=${url}`
-  window.open(downloadUrl)
-})
+    const startTime = startTimeInput.value;
+    const duration = durationInput.value;
+    const url = urlInput.value;
+    const downloadUrl = `http://127.0.0.1:8080/download?url=${url}&start_time=${startTime}&duration=${duration}`;
+    window.open(downloadUrl);
+});
